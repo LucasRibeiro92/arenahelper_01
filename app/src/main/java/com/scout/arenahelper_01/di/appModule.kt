27e2a2eb76e29653a75b14c70b644c29aa93c7e8
LucasRibeiro92 <lucasc.ribeiroo@gmail.com>
2024-07-28 00:29:09 -0300

    Card Search back end implementation.

--- app/src/main/java/com/scout/arenahelper_01/di/appModule.kt
+++ app/src/main/java/com/scout/arenahelper_01/di/appModule.kt
@@ -1,5 +1,7 @@
 package com.scout.arenahelper_01.di
 
+import com.scout.arenahelper_01.data.repository.CardRepository
+import com.scout.arenahelper_01.viewmodel.CardViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
